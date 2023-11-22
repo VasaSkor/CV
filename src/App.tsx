40c6b9406935ba4ae3e5d06c3948df4cda43a3e6
VasaSkor <dzho.ker.700@gmail.com>
2023-11-22 15:05:37 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/App.scss'
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className='App'>
       <Header/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.scss'
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='footer__container'>
+                <h2 className='footer__container-title'>VasaSkor © 2023</h2>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
